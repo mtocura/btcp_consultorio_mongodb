3b2edf6cdc995d82d909befc68e58694647bc16b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-23 17:50:34 -0300

    Adicionando queries

--- src/main/java/br/com/aula3/nosql/consultoriomongodb/repository/TurnRepository.java
+++ src/main/java/br/com/aula3/nosql/consultoriomongodb/repository/TurnRepository.java
@@ -4,6 +4,12 @@ import br.com.aula3.nosql.consultoriomongodb.domain.Turn;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TurnRepository extends MongoRepository<Turn, String> {
+
+  List<Turn> findTurnsByStatus(String status);
+
+  List<Turn> findTurnsByDentist_LastName(String dentistLastName);
 }
