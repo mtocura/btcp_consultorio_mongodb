755f5a8ba9c82e65089bfcfa6d62eb416790adae
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-23 17:10:16 -0300

    Criação do converter para Turn

---
+++ src/main/java/br/com/aula3/nosql/consultoriomongodb/converter/TurnConverter.java
@@ -0,0 +1,41 @@
+package br.com.aula3.nosql.consultoriomongodb.converter;
+
+import br.com.aula3.nosql.consultoriomongodb.domain.Turn;
+import br.com.aula3.nosql.consultoriomongodb.dto.TurnDTO;
+import br.com.aula3.nosql.consultoriomongodb.form.TurnForm;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TurnConverter {
+  private static ModelMapper mapper = new ModelMapper();
+
+  public static Turn turnFormToEntity(TurnForm turnForm) {
+    return mapper.map(turnForm, Turn.class);
+  }
+
+  public List<Turn> turnFormToEntity(List<TurnForm> turnForms) {
+    List<Turn> turns = new ArrayList<>();
+
+    for (TurnForm turnForm : turnForms) {
+      turns.add(turnFormToEntity(turnForm));
+    }
+
+    return turns;
+  }
+
+  public static TurnDTO turnEntityToDTO(Turn turn) {
+    return mapper.map(turn, TurnDTO.class);
+  }
+
+  public List<TurnDTO> turnEntityToDTO(List<Turn> turns) {
+    List<TurnDTO> turnDTOS = new ArrayList<>();
+
+    for (Turn turn : turns) {
+      turnDTOS.add(turnEntityToDTO(turn));
+    }
+
+    return turnDTOS;
+  }
+}
