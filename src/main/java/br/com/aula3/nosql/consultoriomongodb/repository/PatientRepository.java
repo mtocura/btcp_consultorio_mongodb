c0ea7329a9ae6338fa8356109fe1f5fbfb338a36
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-23 17:30:13 -0300

    Adicionando query para obter pacientes pelo sobrenome

--- src/main/java/br/com/aula3/nosql/consultoriomongodb/repository/PatientRepository.java
+++ src/main/java/br/com/aula3/nosql/consultoriomongodb/repository/PatientRepository.java
@@ -4,6 +4,10 @@ import br.com.aula3.nosql.consultoriomongodb.domain.Patient;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends MongoRepository<Patient, String> {
+
+  Optional<Patient> findPatientByLastName(String lastName);
 }
