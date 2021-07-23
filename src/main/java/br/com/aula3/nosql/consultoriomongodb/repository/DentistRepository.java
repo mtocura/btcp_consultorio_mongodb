dfa064b612b096b751c29455cf5c4a794374a3cd
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-23 17:38:24 -0300

    Criação de query para retornar dentistas pelo sobrenome

--- src/main/java/br/com/aula3/nosql/consultoriomongodb/repository/DentistRepository.java
+++ src/main/java/br/com/aula3/nosql/consultoriomongodb/repository/DentistRepository.java
@@ -4,6 +4,10 @@ import br.com.aula3.nosql.consultoriomongodb.domain.Dentist;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DentistRepository extends MongoRepository<Dentist, String> {
+
+  Optional<Dentist> findDentistByLastName(String lastName);
 }
