da3aee9cf2522aba32f4227d60fe7010dbb9091b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-23 16:53:44 -0300

    Ajuste nas annotation do domain Turn

--- src/main/java/br/com/aula3/nosql/consultoriomongodb/domain/Turn.java
+++ src/main/java/br/com/aula3/nosql/consultoriomongodb/domain/Turn.java
@@ -13,15 +13,9 @@ public class Turn {
 
   @Id
   private String id;
-
-  @Field("patient")
   private Patient patient;
-
-  @Field("doctor")
   private Dentist dentist;
-
   private String date;
-
   private String status;
 
   public Turn(Patient patient, Dentist dentist, String date, String status) {
