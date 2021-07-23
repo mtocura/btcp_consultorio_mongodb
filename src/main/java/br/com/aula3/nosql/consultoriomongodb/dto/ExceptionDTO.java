6d8f2629643f726f7f76100d848bc5ee873dc6b6
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-23 17:20:21 -0300

    Criação do DTO para exceção

---
+++ src/main/java/br/com/aula3/nosql/consultoriomongodb/dto/ExceptionDTO.java
@@ -0,0 +1,12 @@
+package br.com.aula3.nosql.consultoriomongodb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDTO {
+  private String message;
+}
