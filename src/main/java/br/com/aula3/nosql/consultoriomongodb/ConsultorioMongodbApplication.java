55d5f8859f77c37f010facc6bb40d92a1dbd9453
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-23 16:34:04 -0300

    Inicialização do projeto spring

---
+++ src/main/java/br/com/aula3/nosql/consultoriomongodb/ConsultorioMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.aula3.nosql.consultoriomongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioMongodbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsultorioMongodbApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/aula3/nosql/consultoriomongodb/ConsultorioMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula3.nosql.consultoriomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioMongodbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
